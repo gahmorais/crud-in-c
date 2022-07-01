aca951f738712ab3c0969e744588d1a898c6dcc1
Gabriel Morais <gmdeveloper01@gmail.com>
2022-07-01 15:40:56 -0300

    refact: Remocao de codigo duplicado

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define DATABASE "USUARIO.txt"
+#define TEMPFILE "temp.txt"
+#define SZ_ID 4
+struct usuario {
+    char nome[40];
+    char id[4];
+} classe_usuario;
+
 void create();
 void read(int i);
 void update();
@@ -10,11 +18,11 @@ void delete ();
 void showAll();
 void opcao();
 void fillWithZeros(char *stringToFill, int lenOfString, int totalLen);
+void idFormat(int id, char *idTemp);
+void saveData(FILE *file, struct usuario user);
+void updateDatabase();
 FILE *openFile(char *file, char *op);
 int getIndex();
-
-#define DATABASE "USUARIO.TXT"
-#define SZ_ID 4
 
 int main() {
     int opt;
@@ -114,103 +122,78 @@ void create() {
     int id = getIndex() + 1;
     char *idTemp = (char *)malloc(SZ_ID);
     char *nome = (char *)malloc(40 * sizeof(char));
-    sprintf(idTemp, "%d", id);                           // Converte o inteiro para string;
-    fillWithZeros(idTemp, strlen(idTemp), (SZ_ID - 1));  // Preenche o id com zeros a esquerda
+    idFormat(id, idTemp);
     printf("Digite o nome do usuário: \n");
-    getchar();  // Utilizando getchar() para consumir o \n que fiz no buffer
+    getchar();  // Utilizando getchar() para consumir o \n que estiver no buffer
     fgets(nome, 30, stdin);
-    fprintf(file, "%s;%s", idTemp, nome);
+    sprintf(classe_usuario.id, "%s", idTemp);
+    sprintf(classe_usuario.nome, "%s", nome);
+    saveData(file, classe_usuario);
     printf("Usuário criado com sucesso!!\n\n");
     free(idTemp);
     fclose(file);
 }
 
 void read(int id) {
-    FILE *file = openFile(DATABASE, "r");
+    FILE *file   = openFile(DATABASE, "r");
     char *idTemp = malloc(SZ_ID);
-    sprintf(idTemp, "%d", id);
-    fillWithZeros(idTemp, strlen(idTemp), (SZ_ID - 1));
-    int ch;
-    char string[40];
-    while (fgets(string, 40, file) != NULL) {
-        if (strstr(string, idTemp)) {
-            printf("\nUsuário encontrado: \n%s\n", string);
+    char *data   = malloc(40 * sizeof(char));
+    idFormat(id, idTemp);
+    while (fgets(data, 40, file)) {
+        if (strstr(data, idTemp)) {
+            printf("\nUsuário encontrado: \n%s\n", data);
+            free(idTemp);
+            free(data);
             return;
         }
     }
     free(idTemp);
+    free(data);
     printf("Usuário não encontrado!!\n");
 }
-
 void update(int id) {
-    char *tempFile = "TEMP.txt";
     FILE *file = openFile(DATABASE, "r");
-    FILE *filetmp = openFile(tempFile, "w");
+    FILE *filetmp = openFile(TEMPFILE, "w");
     char *idTemp = malloc(SZ_ID * sizeof(char));
     char *nome = malloc(40 * sizeof(char));
-    sprintf(idTemp, "%d", id);
-    fillWithZeros(idTemp, strlen(idTemp), (SZ_ID - 1));
-    int ch;
+    idFormat(id, idTemp);
     char string[40];
-    while (fgets(string, 40, file) != NULL) {
+    while (fgets(string, 40, file)) {
         if (strstr(string, idTemp)) {
             printf("Digite o nome do usuário: \n");
-            getchar(); //Consome o \n, caso esteja no buffer, para que a entrada de teclado funcione.
+            getchar();  // Consome o \n, caso esteja no buffer, para que a entrada de teclado funcione.
             fgets(nome, 40, stdin);
             sprintf(string, "%s;%s", idTemp, nome);
+            printf("Usuário Atualizado com sucesso!!\n\n");
         }
-        fprintf(filetmp, "%s", string);
+        fputs(string, filetmp);
     }
     fclose(filetmp);
     fclose(file);
-    file = openFile(DATABASE, "w+");
-    filetmp = openFile(tempFile, "r");
-
-    while (fgets(string, 40, filetmp) != NULL) {
-        fputs(string, file);
-    }
-
-    remove(tempFile);
     free(idTemp);
     free(nome);
-    fclose(file);
-    printf("Usuário Atualizado com sucesso!!\n\n");
+    updateDatabase();
 }
-
 void delete (int id) {
-    int ch;
-    char string[40];
-
-    char *tempFile = "TEMP.txt";
     FILE *file = openFile(DATABASE, "r");
-    FILE *filetmp = openFile(tempFile, "w");
+    FILE *filetmp = openFile(TEMPFILE, "w");
     char *idTemp = malloc(SZ_ID * sizeof(char));
-
-    sprintf(idTemp, "%d", id);
-    fillWithZeros(idTemp, strlen(idTemp), (SZ_ID - 1));
-
-    while (fgets(string, 40, file) != NULL) {
-        if (strstr(string, idTemp)) {
-            sprintf(string, "%s", "");
+    char *nome = malloc(40 * sizeof(char));
+    idFormat(id, idTemp);
+    while (fgets(nome, 40, file)) {
+        if (strstr(nome, idTemp)) {
+            *nome = '\0';
+            printf("Usuário Deletado com sucesso!!\n\n");
         }
-        fprintf(filetmp, "%s", string);
+        fputs(nome, filetmp);
     }
 
     fclose(filetmp);
     fclose(file);
-    file = openFile(DATABASE, "w+");
-    filetmp = openFile(tempFile, "r");
-
-    while (fgets(string, 40, filetmp) != NULL) {
-        fputs(string, file);
-    }
-
-    printf("Usuário Deletado com sucesso!!\n\n");
-    remove(tempFile);
     free(idTemp);
-    fclose(file);
+    free(nome);
+    updateDatabase();
 }
-
 void showAll() {
     FILE *file = openFile(DATABASE, "r");
     int ch;
@@ -219,6 +202,28 @@ void showAll() {
     }
     fclose(file);
 }
+
+void updateDatabase() {
+    FILE *file = openFile(DATABASE, "w+");
+    FILE *filetmp = openFile(TEMPFILE, "r");
+    char string[40];
+
+    while (fgets(string, 40, filetmp)) {
+        fputs(string, file);
+    }
+    fclose(filetmp);
+    fclose(file);
+    remove(TEMPFILE);
+}
+
+void idFormat(int id, char *idStr) {
+    sprintf(idStr, "%d", id);
+    fillWithZeros(idStr, strlen(idStr), (SZ_ID - 1));
+}
+
+void saveData(FILE *file, struct usuario user) {
+    fprintf(file, "%s;%s", user.id, user.nome);
+};
 
 void fillWithZeros(char *stringToFill, int lenOfString, int totalLen) {
     int count;
